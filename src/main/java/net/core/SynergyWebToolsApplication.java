ea354e68770a5f6809f8f16048d65eaee3eeb029
Arsyad2009 <105086466+Arsyad2009@users.noreply.github.com>
2024-08-07 22:09:36 +0700

    Fix port configuration for Railway

--- src/main/java/net/core/SynergyWebToolsApplication.java
+++ src/main/java/net/core/SynergyWebToolsApplication.java
@@ -3,9 +3,10 @@ package net.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@SpringBootApplication
-	public class SynergyWebToolsApplication {
-		public static void main(String[] args) {
-			SpringApplication.run(SynergyWebToolsApplication.class, args);
-		}
+@SpringBootApplication
+public class SynergyWebToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyWebToolsApplication.class, args);
 	}
+}
